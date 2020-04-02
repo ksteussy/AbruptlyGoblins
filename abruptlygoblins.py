9002fa682f969c7f3404fc2fcef955ba342537b8
ksteussy <ksteuss@gmail.com>
2020-04-01 20:05:02 -0500

    final

--- abruptlygoblins.py
+++ abruptlygoblins.py
@@ -1,9 +1,17 @@
 gamers = []
+form_email = """
+Dear {name},
+
+The Sorcery Society is happy to host "{game}" night and wishes you will attend. Come by on {day_of_week} and have a blast!
+
+Magically Yours,
+the Sorcery Society
+"""
 
 
 def add_gamer(gamer, gamers_list):
     if 'name' and 'availability' in gamer:
-        gamers.append(gamer)
+        gamers_list.append(gamer)
 
 
 def build_daily_frequency_table():
@@ -13,17 +21,30 @@ def build_daily_frequency_table():
 def calculate_availability(gamers_list, available_frequency):
     for gamer in gamers_list:
         for day in gamer['availability']:
-            count_availability[day] += 1
+            available_frequency[day] += 1
 
 
 def find_best_night(availability_table):
     largest = float('-inf')
-    best_night = ""
     for day in availability_table:
         if availability_table[day] > largest:
             largest = availability_table[day]
             best_night = day
     return best_night
+
+
+def available_on_night(gamers_list, day):
+    available = []
+    for gamer in gamers_list:
+        for item in gamer['availability']:
+            if item == day:
+                available.append(gamer['name'])
+    return available
+
+
+def send_email(gamers_who_can_attend, day, game):
+    for gamer in gamers_who_can_attend:
+        print(form_email.format(name=gamer, day_of_week=day, game=game))
 
 
 kimberly = {'name': 'Kimberly Warner', 'availability': ['Monday', 'Tuesday', 'Friday']}
@@ -41,6 +62,13 @@ add_gamer({'name': 'Michel Trujillo', 'availability': ["Monday", "Tuesday", "Wed
 count_availability = build_daily_frequency_table()
 calculate_availability(gamers, count_availability)
 game_night = find_best_night(count_availability)
-print(game_night)
+attending_game_night = available_on_night(gamers, game_night)
+send_email(attending_game_night, game_night, "Abruptly Goblins!")
 
+unable_to_attend_best_night = [gamer for gamer in gamers if gamer['name'] not in attending_game_night]
 
+second_night_availability = build_daily_frequency_table()
+calculate_availability(unable_to_attend_best_night, second_night_availability)
+second_night = find_best_night(second_night_availability)
+available_second_game_night = available_on_night(gamers, second_night)
+send_email(available_second_game_night, second_night, "Abruptly Goblins!")
